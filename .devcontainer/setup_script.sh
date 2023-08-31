0570b1591bedfb702561a8f314650f95b916b954
Gabriel Rosa <gabriel.rosa@dadosfera.ai>
2023-08-31 17:35:57 -0300

    init

---
+++ .devcontainer/setup_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# sudo apt-get update
+# sudo apt-get install graphviz -y
+
+# Install any dependencies you have in this shell script using mamba run -n py38 pip install
+# mamba run -n py38 pip install scikit-learn graphviz joblib
